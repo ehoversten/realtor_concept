14fd2374b5d1931fd83e366bd6a48d3c0950ea17
ehoversten <ehoversten@gmail.com>
2018-11-15 22:20:26 -0500

    added dynamic content to about page

--- pages/views.py
+++ pages/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, redirect, HttpResponse
 
 
 from listings.models import Listing
+from realtors.models import Realtor
+
 # Create your views here.
 def index(request):
+    # Get 3 Latest Listings
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
     context = {
         'listings':listings
@@ -12,7 +15,14 @@ def index(request):
 
 
 def about(request):
-    context = {
+    # Get Realtors
+    realtors = Realtor.objects.order_by('-hire_date')
 
+    # Get MVP Realtor
+    mvp_realtor = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors':realtors,
+        'mvp_realtor': mvp_realtor
     }
     return render(request, 'pages/about.html', context)
