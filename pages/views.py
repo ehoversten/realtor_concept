5949217574871c5647a4c3b1e23c7259a7192026
ehoversten <ehoversten@gmail.com>
2018-11-15 21:20:56 -0500

    added dynamic content to featured listings on homepage

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect, HttpResponse
 
+
+from listings.models import Listing
 # Create your views here.
 def index(request):
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
     context = {
-    
+        'listings':listings
     }
     return render(request, 'pages/index.html', context)
 
