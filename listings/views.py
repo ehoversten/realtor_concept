19371fbb67565c157f8fce455c4f02e07d2fd1fd
ehoversten <ehoversten@gmail.com>
2018-11-21 23:10:39 -0500

    connected form inputs to serach listings queryset

--- listings/views.py
+++ listings/views.py
@@ -27,9 +27,52 @@ def listing(request, listing_id):
 
 
 def search(request):
+    queryset_list = Listing.objects.order_by('-list_date')
+    # queryset_list = Listing.objects.all()
+
+    # Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        print(keywords)
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+
+    # City 
+    if 'city' in request.GET:
+        city = request.GET['city']
+        print(city)
+        if city:
+            queryset_list = queryset_list.filter(
+                city__iexact=city)
+
+    # State
+    if 'state' in request.GET:
+        state = request.GET['state']
+        print(state)
+        if state:
+            queryset_list = queryset_list.filter(
+                state__iexact=state)
+
+    # Bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        print(bedrooms)
+        if bedrooms:
+            queryset_list = queryset_list.filter(
+                bedrooms__lte=bedrooms)
+
+    # Price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        print(price)
+        if price:
+            queryset_list = queryset_list.filter(
+                price__lte=price)
+
     context = {
         'state_choices'  : state_choices,
         'price_choices'  : price_choices,
         'bedroom_choices': bedroom_choices,
+        'listings'       : queryset_list,
     }
     return render(request, 'listings/search.html', context)
