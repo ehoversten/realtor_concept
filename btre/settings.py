2a24d7599c9a55906f5a6f0efff2c4e6563a9671
ehoversten <ehoversten@gmail.com>
2018-11-26 16:52:03 -0500

    added contacts app and registered with admin.py

--- btre/settings.py
+++ btre/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
     'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
 ]
 
 MIDDLEWARE = [
